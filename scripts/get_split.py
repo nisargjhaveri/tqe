27d713e82aa5f785d77e8eb69c1e46447901afa7
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2018-04-01 15:28:52 +0530

    Add a script to get test/dev/train split from file

---
+++ scripts/get_split.py
@@ -0,0 +1,47 @@
+import argparse
+
+import sys
+import numpy as np
+from sklearn.model_selection import ShuffleSplit
+
+
+def loadFile(inFile):
+    return np.array(
+                map(lambda l: l.decode('utf-8'), list(inFile)),
+                dtype=object
+            )
+
+
+def printFile(lines):
+    sys.stdout.writelines(lines)
+
+
+def split(inFile, split):
+    lines = loadFile(inFile)
+
+    splitter = ShuffleSplit(n_splits=1, test_size=.2, random_state=42)
+    train_index, dev_index = splitter.split(lines).next()
+
+    dev_len = len(dev_index) / 2
+
+    if split == "train":
+        printFile(lines[train_index])
+    elif split == "dev":
+        printFile(lines[dev_index[:dev_len]])
+    elif split == "test":
+        printFile(lines[dev_index[dev_len:]])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+            description='Train Translation Quality Estimation')
+
+    parser.add_argument('split', type=str, choices=['train', 'dev', 'test'],
+                        help='Which split to generate')
+
+    parser.add_argument('input_file', type=file,
+                        help='File to split')
+
+    args = parser.parse_args()
+
+    split(args.input_file, args.split)
